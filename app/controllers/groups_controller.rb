6e91b229fb066a85f9964f8d558db41277891c3f
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-02-12 13:56:49 +0900

    modified some places using rubocop

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,22 +7,21 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    return redirect_to @group, alert:'グループの作成に失敗しました！' unless @group.save
+    return redirect_to @group, alert: 'グループの作成に失敗しました！' unless @group.save
     redirect_to @group, notice: 'グループの作成に成功しました！'
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    return redirect_to @group, alert:'グループの更新に失敗しました！' unless @group.update(group_params)
+    return redirect_to @group, alert: 'グループの更新に失敗しました！' unless @group.update(group_params)
     redirect_to @group, notice: 'グループの更新に成功しました！'
   end
 
   private
 
   def group_params
-    params.require(:group).permit(:name, { user_ids: [] })
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ActiveRecord::Base
 
   has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
-
 end
