bba2bacc67467299dde34c0634a0c5e6a47b93cd
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-02-12 12:28:23 +0900

    define new, create, edit and update actions in groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,32 @@
 class GroupsController < ApplicationController
+  bofore_action :set_group, only: [:edit, :update]
+
   def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    return, alert:'グループの作成に失敗しました！' unless @group.save
+    redirect_to @group, notice: 'グループの作成に成功しました！'
   end
 
   def edit
+  end
+
+  def update
+    @group = Group.new(group_params)
+    return, alert:'グループの更新に失敗しました！' unless @group.save
+    redirect_to @group, notice: 'グループの更新に成功しました！'
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
