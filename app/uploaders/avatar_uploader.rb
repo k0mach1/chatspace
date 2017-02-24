40dc99885931402f3a00d48cd1e6e022b1bdff6a
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-02-24 16:05:24 +0900

    modified some places with rubocop

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,17 +8,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
-  # 格納するディレクトリを指定
-  def store_dir
-    model.id.to_s
-  end
-
   # キャッシュを格納ディレクトリを指定
   def cache_dir
     'cache'
   end
-  # storage :fog
 
+  storage :fog
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
